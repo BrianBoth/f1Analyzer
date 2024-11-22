15788eb79a464a19f970b158a73ebe17dbd76481
Brian Both <bboth@uci.edu>
2024-11-21 21:09:30 -0800

    added connection to database

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.get("/", (request, response) => {
-  console.log(request);
   return response.status(234).send("Its Working!");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port ${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App Connected to Database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
