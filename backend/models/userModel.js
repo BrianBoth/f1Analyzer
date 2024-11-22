eaaab9daa2591bb12fd3f49f670618543d5c695c
Brian Both <bboth@uci.edu>
2024-11-22 11:02:27 -0800

    created mongodb Schemas

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,1 +1,18 @@
-// create user schema
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
--- backend/models/videoDataModel.js
+++ backend/models/videoDataModel.js
@@ -1,1 +1,14 @@
-// create video data model
+import mongoose from "mongoose";
+
+const videoShema = mongoose.Schema({
+  userID: {
+    type: String,
+    required: true,
+  },
+  videoData: {
+    type: Array,
+    required: true;
+  }
+});
+
+export const userVideoData = mongoose.model("userVideoData", videoShema);
