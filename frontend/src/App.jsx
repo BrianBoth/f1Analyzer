dff64d10f1ee88175cbc2f895ad8e7c097168f8e
Brian Both <bboth@uci.edu>
2024-11-23 10:54:02 -0800

    created jsx pages and fixed server issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage.jsx";
+import Login from "./pages/LoginPage.jsx";
+import Signup from "./pages/SignupPage.jsx";
+import UserVideos from "./pages/UserVideos.jsx";
+import DisplayVideo from "./pages/DisplayVideo.jsx";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/signup" element={<Signup></Signup>}></Route>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/data/:id" element={<UserVideos></UserVideos>}></Route>
+      <Route
+        path="/display/:id"
+        element={<DisplayVideo></DisplayVideo>}
+      ></Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/UserVideos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserVideos = () => {
+  return <div>UserVideos</div>;
+};
+
+export default UserVideos;
